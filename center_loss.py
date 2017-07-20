26118eea37419f042235d83a076c9ab16c4ef176
Shunsuke KITADA <septemtrio.ager@gmail.com>
2017-07-21 00:37:54 +0900

    [WIP] update center_loss.py and train_mnist.py

--- center_loss.py
+++ center_loss.py
@@ -2,6 +2,7 @@
 
 import numpy
 
+from chainer import cuda
 from chainer import function
 from chainer.utils import type_check
 
@@ -10,16 +11,29 @@ class CenterLossFunction(function.Function):
 
     """Center loss function."""
 
+    def __init__(self, num_classes):
+        self.num_classes = num_classes
+
     def check_type_forward(self, in_types):
-        pass
+        x_type, t_type = in_types
+        type_check.expect(in_types.size() == 2)
+        type_check.expect(
+            x_type.dtype == numpy.float32,
+            t_type.dtype == numpy.int32,
+            x_type.shape[0] == t_type.shape[0]
+        )
 
     def forward(self, inputs):
+        xp = cuda.get_array_module(*inputs)
+        features, labels = inputs
+
+    def backward(self, inputs, gy):
         pass
 
 
-def center_loss_function(x, alpha):
+def center_loss_function(x, t, num_classes):
     """Center loss function.
 
     This function computes center loss.
     """
-    return CenterLossFunction(alpha)(x)
+    return CenterLossFunction(num_classes=num_classes)(x, t)
--- train_mnist.py
+++ train_mnist.py
@@ -1,8 +1,107 @@
 # -*- coding: utf-8 -*-
 
+import argparse
+
+import chainer
+import chainer.functions as F
+from chainer import training
+from chainer.training import extensions
+
+from model import LeNets
+from util.logger import setup_logger
+
 
 def main():
-    pass
+
+    parser = argparse.ArgumentParser(description='Chainer example: MNIST')
+    parser.add_argument('--batchsize', '-b', type=int, default=100,
+                        help='Number of images in each mini-batch')
+    parser.add_argument('--epoch', '-e', type=int, default=20,
+                        help='Number of sweeps over the dataset to train')
+    parser.add_argument('--frequency', '-f', type=int, default=-1,
+                        help='Frequency of taking a snapshot')
+    parser.add_argument('--gpu', '-g', type=int, default=-1,
+                        help='GPU ID (negative value indicates CPU)')
+    parser.add_argument('--out', '-o', default='result',
+                        help='Directory to output the result')
+    parser.add_argument('--resume', '-r', default='',
+                        help='Resume the training from snapshot')
+    args = parser.parse_args()
+
+    logger = setup_logger(__name__)
+    logger.info("GPU: {}".format(args.gpu))
+    logger.info("# Minibatch-size: {}".format(args.batchsize))
+    logger.info("# epoch: {}".format(args.epoch))
+
+    NUM_CLASSES = 10
+
+    model = LeNets(out_dim=NUM_CLASSES, lambda_ratio=0.5)
+    if args.gpu >= 0:
+        chainer.cuda.get_device_from_id(args.gpu).use()
+        model.to_gpu()
+
+    if args.gpu >= 0:
+        # Make a specified GPU current
+        chainer.cuda.get_device_from_id(args.gpu).use()
+        model.to_gpu()  # Copy the model to the GPU
+
+    # Setup an optimizer
+    optimizer = chainer.optimizers.Adam()
+    optimizer.setup(model)
+
+    # Load the MNIST dataset
+    train, test = chainer.datasets.get_mnist(ndim=3)
+
+    train_iter = chainer.iterators.SerialIterator(train, args.batchsize)
+    test_iter = chainer.iterators.SerialIterator(test, args.batchsize,
+                                                 repeat=False, shuffle=False)
+
+    # Set up a trainer
+    updater = training.StandardUpdater(train_iter, optimizer, device=args.gpu)
+    trainer = training.Trainer(updater, (args.epoch, 'epoch'), out=args.out)
+
+    # Evaluate the model with the test dataset for each epoch
+    trainer.extend(extensions.Evaluator(test_iter, model, device=args.gpu))
+
+    # Dump a computational graph from 'loss' variable at the first iteration
+    # The "main" refers to the target link of the "main" optimizer.
+    trainer.extend(extensions.dump_graph('main/loss'))
+
+    # Take a snapshot for each specified epoch
+    frequency = args.epoch if args.frequency == -1 else max(1, args.frequency)
+    trainer.extend(extensions.snapshot(), trigger=(frequency, 'epoch'))
+
+    # Write a log of evaluation statistics for each epoch
+    trainer.extend(extensions.LogReport())
+
+    # Save two plot images to the result dir
+    if extensions.PlotReport.available():
+        trainer.extend(
+            extensions.PlotReport(['main/loss', 'validation/main/loss'],
+                                  'epoch', file_name='loss.png'))
+        trainer.extend(
+            extensions.PlotReport(
+                ['main/accuracy', 'validation/main/accuracy'],
+                'epoch', file_name='accuracy.png'))
+
+    # Print selected entries of the log to stdout
+    # Here "main" refers to the target link of the "main" optimizer again, and
+    # "validation" refers to the default name of the Evaluator extension.
+    # Entries other than 'epoch' are reported by the Classifier link, called by
+    # either the updater or the evaluator.
+    trainer.extend(extensions.PrintReport(
+        ['epoch', 'main/loss', 'validation/main/loss',
+         'main/accuracy', 'validation/main/accuracy', 'elapsed_time']))
+
+    # Print a progress bar to stdout
+    trainer.extend(extensions.ProgressBar())
+
+    if args.resume:
+        # Resume from a snapshot
+        chainer.serializers.load_npz(args.resume, trainer)
+
+    # Run the training
+    trainer.run()
 
 
 if __name__ == '__main__':
